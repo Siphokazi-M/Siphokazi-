13b806028df142d9892262ceec2d957d8b3643ad
Siphokazi-M <149484512+Siphokazi-M@users.noreply.github.com>
2023-11-14 16:41:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showWeather);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+  
+  async function showWeather(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+  
+    // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+    const apiKey = "f3548597b68d84adc634d43be05fd24a";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  
+    try {
+      const response = await axios.get(apiUrl);
+      const data = response.data;
+  
+      const locationElement = document.getElementById("location");
+      const conditionElement = document.getElementById("condition");
+      const temperatureElement = document.getElementById("temperature");
+      const countryElement = document.getElementById("country");
+  
+      locationElement.textContent = `Location: ${data.name}`;
+      conditionElement.textContent = `Condition: ${data.weather[0].description}`;
+      temperatureElement.textContent = `Temperature: ${data.main.temp}°C`;
+      countryElement.textContent = `Country Code: ${data.sys.country}`;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
